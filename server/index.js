7572c3d67fd25d4a3e908775b5d8d946570afdde
Ge, Luwei <Luwei.Ge@ncr.com>
2019-07-24 23:09:51 -0400

    implemented the basic service. added a Dockerfile which has not been tested

--- server/index.js
+++ server/index.js
@@ -4,13 +4,18 @@
 const path = require('path');
 const protoLoader = require('@grpc/proto-loader');
 const grpc = require('grpc');
+const os = require('os');
 
 // grpc service definition
 const pingPongServiceProtoPath = path.join(__dirname, '..', 'ping-pong.proto');
 const pingPongServiceProtoDefinition = protoLoader.loadSync(pingPongServiceProtoPath);
 const pingPongServicePackageDefinition = grpc.loadPackageDefinition(pingPongServiceProtoDefinition);
 
-function PingPong() {}
+function PingPong(call, callback) {
+    const ping = call.request.ping;
+    let pong = `Hello, ${ping}! This is ${os.hostname()}.`;
+    callback(null, {pong: pong});
+}
 
 function main() {
     const server = new grpc.Server();
@@ -22,7 +27,7 @@ function main() {
     // gRPC server
     server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
     server.start();
-    console.log('gRPC server running at http://0.0.0.0:50051');
+    console.log('PingPong gRPC server running at http://0.0.0.0:50051');
 }
 
 main();
